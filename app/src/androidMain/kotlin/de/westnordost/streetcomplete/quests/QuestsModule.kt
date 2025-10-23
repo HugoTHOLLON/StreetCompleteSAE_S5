2e1a51678742d1ac7e08c839f7739a4f15774faf
ElPoraz <pablo.marquet@gmail.com>
2025-10-23 21:14:01 +0200

    Adding the corrected quest "kind of lock" in master

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/QuestsModule.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/QuestsModule.kt
@@ -43,7 +43,7 @@ import de.westnordost.streetcomplete.quests.bike_shop.AddBikeRepairAvailability
 import de.westnordost.streetcomplete.quests.bike_shop.AddSecondHandBicycleAvailability
 import de.westnordost.streetcomplete.quests.board_name.AddBoardName
 import de.westnordost.streetcomplete.quests.board_type.AddBoardType
-import de.westnordost.streetcomplete.quests.boat_lock_type.AddBoatLockType
+import de.westnordost.streetcomplete.quests.boat_lock_self_service.AddBoatLockSelfService
 import de.westnordost.streetcomplete.quests.boat_rental.AddBoatRental
 import de.westnordost.streetcomplete.quests.bollard_type.AddBollardType
 import de.westnordost.streetcomplete.quests.bothway.AddAerialBothWay
@@ -342,7 +342,7 @@ fun questTypeRegistry(
     43 to AddTrafficSignalsButton(),
     44 to AddTrafficSignalsVibration(),
 
-    186 to AddBoatLockType(),
+    186 to AddBoatLockSelfService(),
 
     /* ↓ 2.solvable when right in front of it ----------------------------------------------- */
     45 to AddInformationToTourism(), // OSM Carto
--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/boat_lock_type/BoatLockType.kt
+++
@@ -1,3 +0,0 @@
-package de.westnordost.streetcomplete.quests.boat_lock_type
-
-enum class BoatLockType { AUTOMATED, SELF_SERVICE, MANUAL }
